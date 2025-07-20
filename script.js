482628112aeb40a9ddb19df075aa558d96bd63d3
TheOnlyMoumen <theonlymoumen@gmail.com>
2025-07-20 01:12:39 +0100

    Add calculator logic
    - Add calculator logic and history preview.
    - Handle keyboard input (some issues are still present and will be fixed in later versions).
    - Add a mode switcher (the advanced mode will be added in later versions)

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ function changeTheme() {
 
 class Calculator {
     #display;
+    #history;
     #modes;
     #dropDown;
     #dropDownBtn;
@@ -27,9 +28,15 @@ class Calculator {
     #clearBtn;
     #submitBtn;
     #displayBtns;
+    #operators;
+
+    #result;
+    #isNewExpression;
+    #startEval;
 
     constructor () {
         this.#display = document.getElementById("display");
+        this.#history = document.getElementById("history");
         this.#modes = Array.from(document.querySelectorAll("input[name='mode']"));
         this.#modes[0].checked = true;
 
@@ -40,9 +47,17 @@ class Calculator {
 
         this.#clearBtn = document.querySelector(".calc-btn-clear");
         this.#submitBtn = document.querySelector(".calc-btn-submit");
-        this.#displayBtns = Array.from(document.querySelectorAll(".calc-btn-number, .calc-btn-period, .calc-btn-left-p, .calc-btn-right-p, .calc-btn-operator"));
+        this.#operators = Array.from(document.querySelectorAll(".calc-btn-operator, .calc-btn-percent"));
+        this.#displayBtns = Array.from(document.querySelectorAll(".calc-btn-number, .calc-btn-period," + 
+            ".calc-btn-left-p, .calc-btn-right-p, .calc-btn-operator, .calc-btn-pi, .calc-btn-percent," +
+            ".calc-btn-sqrt, .calc-btn-square"));
 
         this.#dropDown.setAttribute("data-mode", this.#modes[0].value);
+
+        this.#result = 0;
+        this.#isNewExpression = true;
+        this.#startEval = false;
+
     }
 
     #handleEvents() {
@@ -75,14 +90,119 @@ class Calculator {
 
         // Clear the display if AC button is clicked
         this.#clearBtn.addEventListener("click", () => this.#display.value = "");
+        
+        // Prevent new lines from appearing in the display
+        this.#display.addEventListener("keydown", (event) => {
+            if (event.key === "Enter")
+                event.preventDefault();
+        })
 
         // Display the value of the keys when pressed
+        const displayValue = (btn) => {
+            if (this.#isNewExpression && this.#isValidResult() && this.#operators.includes(btn)) {
+                this.#display.value += btn.innerText;
+                this.#isNewExpression = false;
+            } else if (this.#isNewExpression) {
+                this.#display.value = (btn.innerText === "√") ? btn.innerText + "(" : btn.innerText;
+                this.#isNewExpression = false;
+            } else
+                this.#display.value += (btn.innerText === "√") ? btn.innerText + "(" : btn.innerText;
+        }
+
         for (const btn of this.#displayBtns) {
             btn.addEventListener("click", () => {
-                this.#display.value += btn.innerHTML;
+                displayValue(btn);
             })
         }
 
+        // Handle keyboard events
+        addEventListener("keydown", (event) => {
+            if (event.key === "Enter" && this.#display.value !== ""){
+                this.#display.value = this.#evaluate(this.#display.value);
+                this.#isNewExpression = true;
+            } else if (event.key === "Escape") {
+                this.#display.value = "";
+                this.#isNewExpression = true;
+            } else {
+                this.#display.focus();
+                this.#isNewExpression = false;
+            }
+        })
+
+        // Evaluate the expression when '=' is clicked
+        this.#submitBtn.addEventListener("click", () => {
+            if (this.#display.value !== "") {
+                this.#display.value = this.#evaluate(this.#display.value);
+                this.#isNewExpression = true;
+            }
+        })
+
+        // Display history expression when clicked
+        const observer = new MutationObserver(() => {
+            this.#history.querySelectorAll("div").forEach(item => {
+                item.addEventListener("click", () => {
+                    this.#display.value = item.querySelector(".expression").getAttribute("value").toString();
+                    this.#isNewExpression = false;
+                })
+            })
+        })
+
+        observer.observe(this.#history, {
+            childList: true
+        })
+    }
+
+    #isValidResult() {
+        return (
+            typeof this.#result === "number" &&
+            isFinite(this.#result)
+        );
+    }
+
+    #evaluate(expression) {
+        let evalExpression = expression.replace(/[+−×÷π%√²]/g, match => {
+            const replacements = {
+                '+': '+',
+                '−': '-',
+                '×': '*',
+                '÷': '/',
+                'π': '(' + Math.PI.toString() + ')',
+                '%': '*(0.01)',
+                '√': 'sqrt',
+                '²': '^(2)'
+            };
+            return replacements[match];
+        });
+
+        evalExpression = evalExpression.replace(/mod/g, '%');
+        expression = expression.replace(/\s+/g, "");
+        
+
+        try {
+            this.#result = math.evaluate(evalExpression);
+            if (this.#isValidResult()) {
+                let historyExpression = (expression.length > 10) ?
+                expression.slice(0, 10) + "..." : expression;
+                let historyResult = (this.#result.toString().length > 10) ?
+                this.#result.toString().slice(0, 10) + "..." : this.#result.toString();
+
+                this.#history.insertAdjacentHTML("afterbegin", 
+                    "<div><span class='expression' value='" + expression + "'>" + 
+                    historyExpression + "</span><span class='equal-sign'> = </span>" + 
+                    "<span class='result'>" + historyResult + 
+                    "</span></div>"
+                );
+                return this.#result;
+            } else {
+                this.#result = null;
+                return "Math Error";
+            }
+        }
+
+        catch (err) {
+            this.#result = null;
+            return "Syntax Error";
+        }
     }
 
     _run(flag) {
